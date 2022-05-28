7b9bda0907e55c854af7259a6b95abb86b9378c3
WIllian Paulino <willianpaulinox@gmail.com>
2022-05-28 20:44:51 -0300

    Finalized the TypeScript module, with an introduction to this resource that we can use to build a more under control js-based applications.

--- src/3-poo/classes-heranca.ts
+++ src/3-poo/classes-heranca.ts
@@ -20,3 +20,9 @@ const cao = new Animal ('cannis caramelo', 2);
 
 console.log(cao.checaPerigo())
 console.log(Animal.definicao())
+
+/**Diferenças entre public, private and protected 
+ * public: propriedade acessível dentro e fora da classe. Da pra modificar seu valor fora da classe.
+ * private: propriedade acessível somente dentro da classe.
+ * protected: propriedade acessível dentro da classe e das subclasses.
+*/
